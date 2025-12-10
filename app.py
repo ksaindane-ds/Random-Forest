7d4d87597d151432777430ffc2f1f8184235ffcd
KrushnaSaindane17 <krushnasaindane875@gmail.com>
2025-12-10 14:03:28 +0530

    app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def main():
     
 
 # #---Done --
-#     st.markdown("Developed by External Guide Avinash Pawar and WBL intern : Sana Khan at NIELIT Daman")
+st.markdown("Developed by Krushna Saindane...!!")
 
 if __name__ == '__main__':
     main()
